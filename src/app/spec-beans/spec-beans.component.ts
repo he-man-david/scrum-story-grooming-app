6cb7111b58ca3aabe72801710e31dccc6866788f
davidhe184 <david.he184@gmail.com>
2018-07-28 18:19:28 -0500

    added selected board, and bean counts

--- src/app/spec-beans/spec-beans.component.ts
+++ src/app/spec-beans/spec-beans.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SpecBeansComponent implements OnInit {
   @Input() specName: string; 
+  @Input() count: number;
 
   constructor() { }
 
--- src/app/spec-board/spec-board.component.ts
+++ src/app/spec-board/spec-board.component.ts
@@ -9,12 +9,23 @@ import  * as specData from '../specData.json';
 export class SpecBoardComponent implements OnInit {
   developmentSpecs: string[];
   testingSpecs: string[];
+  selectedSpecsArr: string[] = [];
+  selectedSpecsObj: object = {};
 
   constructor() { }
 
   ngOnInit() {
     this.developmentSpecs = Object.keys(specData.development);
     this.testingSpecs = Object.keys(specData.testing);
+  }
+
+  selectedSpec(spec) {
+    if (this.selectedSpecsObj[spec]) {
+      this.selectedSpecsObj[spec]++;
+    } else {
+      this.selectedSpecsObj[spec] = 1;
+      this.selectedSpecsArr.push(spec)
+    }
   }
 
 }
