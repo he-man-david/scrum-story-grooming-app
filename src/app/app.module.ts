8ce62d4da2c9e478059b4c48720f785ea3efe9f6
davidhe184 <david.he184@gmail.com>
2018-08-04 16:27:17 -0500

    added routing module to app module router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { SpecBoardComponent } from './spec-board/spec-board.component';
 import { SpecBeansComponent } from './spec-beans/spec-beans.component';
 import { ParticlesModule } from 'angular-particle';
+import { EditBoardComponent } from './edit-board/edit-board.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SpecBoardComponent,
-    SpecBeansComponent
+    SpecBeansComponent,
+    EditBoardComponent
   ],
   imports: [
     BrowserModule,
-    ParticlesModule
+    ParticlesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
