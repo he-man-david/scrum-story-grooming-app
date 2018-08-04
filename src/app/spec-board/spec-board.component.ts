5f72d97d58a95d088c55689d76c381fac75e391f
davidhe184 <david.he184@gmail.com>
2018-08-04 14:25:43 -0500

    added totalpoint computation select and deselect

--- src/app/spec-board/spec-board.component.ts
+++ src/app/spec-board/spec-board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  * as specData from '../specData.json';
+import  { specData } from '../specData';
 
 @Component({
   selector: 'spec-board',
@@ -9,14 +9,17 @@ import  * as specData from '../specData.json';
 export class SpecBoardComponent implements OnInit {
   developmentSpecs: string[];
   testingSpecs: string[];
+  allSpecs: object;
   selectedSpecsArr: string[] = [];
   selectedSpecsObj: object = {};
+  totalPoint: number = 0;
 
   constructor() { }
 
   ngOnInit() {
     this.developmentSpecs = Object.keys(specData.development);
     this.testingSpecs = Object.keys(specData.testing);
+    this.allSpecs = { ...specData.development, ...specData.testing };
   }
 
   selectedSpec(spec) {
@@ -24,8 +27,34 @@ export class SpecBoardComponent implements OnInit {
       this.selectedSpecsObj[spec]++;
     } else {
       this.selectedSpecsObj[spec] = 1;
-      this.selectedSpecsArr.push(spec)
+      this.selectedSpecsArr.push(spec);
     }
+    this.computingPoints(spec);
+  }
+
+  deselectSpec(spec) {
+    if (this.selectedSpecsObj[spec] && this.selectedSpecsObj[spec] !== 1) {
+      this.selectedSpecsObj[spec]--;
+    } else {
+      this.selectedSpecsObj[spec] = 0;
+      this.selectedSpecsArr.splice(this.selectedSpecsArr.indexOf(spec), 1);
+    }
+    this.computingPoints(spec, "-");
+  }
+
+  computingPoints(spec, operation = "+") {
+    if (operation === "+") {
+      this.totalPoint += this.allSpecs[spec];
+    } else {
+      this.totalPoint -= this.allSpecs[spec];
+    }
+  }
+
+  clearAll() {
+    console.log("hey", this.totalPoint);
+    this.totalPoint = 0;
+    this.selectedSpecsArr = [];
+    this.selectedSpecsObj = {};
   }
 
 }
