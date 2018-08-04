0752574c6a3eb25cb72da75d3727d40f667ce2bb
davidhe184 <david.he184@gmail.com>
2018-08-04 14:25:24 -0500

    styling and added selecteBean function to spec bean

--- src/app/spec-beans/spec-beans.component.ts
+++ src/app/spec-beans/spec-beans.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'spec-beans',
   templateUrl: './spec-beans.component.html',
   styleUrls: ['./spec-beans.component.css']
 })
+
 export class SpecBeansComponent implements OnInit {
   @Input() specName: string; 
   @Input() count: number;
+  @Input() color: string;
+  @Output() selected: EventEmitter<string> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectSpec(spec) {
+    this.selected.emit(spec)
+  }
+
 }
+
+
